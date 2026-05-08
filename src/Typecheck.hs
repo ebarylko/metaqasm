f2e240aae7dfe3d9206e2289803cec1313e7e3e1
Eitan Barylko <eitan@barylko.com>
2026-05-08 14:35:12 -0700

    Added the first test

--- src/Typecheck.hs
+++ src/Typecheck.hs
@@ -1,27 +1,58 @@
 module Typecheck
-    ( someFunc
+    ( Expression(..),
+      EvaluationContext,
+      determineType,
+      Identifier,
+      TermType(..),
+      TypeCalculationResult
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Map as M
+
+{-@ type Index  = Nat @-}
+
+type Identifier = String
+
+type EvaluationContext = M.Map Identifier TermType
+
+-- This data type represents the values an expression can take on,
+-- being either a reference to another term or an attempt to obtain a bit or qubit from a
+-- collection of registers
+{-@ data Expression = Identifier | RegisterAccess{registerName::Identifier,  registerNumber::Index} @-}
+data Expression = Identifier | RegisterAccess{registerName::Identifier,  registerNumber::Int} deriving (Show, Eq)
+
+{-@ newtype RegistersInfo = RegistersInfo{registerName:: Identifier, numOfRegisters:: NonNegative} deriving (Show, Eq) @-}
+
+-- This data type represents the possible commands a user can execute, including the creation of
+-- n classical or quantum registers accessible under a certain name
+{-@  data Command = DeclareQuantumRegisters RegistersInfo | DeclareClassicalRegisters RegistersInfo deriving (Show, Eq) @-}
 
 
- -- newtype NonNegative {v:: Int}
- -- 
- -- type Identifier = String
- -- 
- -- type Index = NonNegative
- -- 
- -- -- This data type represents the values an expression can take on,
- -- -- being either a reference to another term or an attempt to obtain a bit or qubit from a
- -- -- collection of registers
- -- data Expression = Identifier | RegisterAccess{registerName::Identifier,  registerNumber::Index}
- -- 
- -- newtype RegistersInfo = RegistersInfo{registerName:: Identifier, numOfRegisters:: NonNegative}
- -- 
- -- 
- -- -- This data type represents the possible commands a user can execute, including the creation of
- -- -- n classical or quantum registers accessible under a certain name
- -- data Command = DeclareQuantumRegisters RegistersInfo | DeclareClassicalRegisters RegistersInfo
- -- 
- -- data termType = Bit | Qbit | 
+data TermType
+  = Bit
+  | Qbit
+  | QuantumRegisters Int
+  | ClassicalRegisters Int
+  deriving (Show, Eq)
+
+{-@
+data TermType
+  = Bit
+  | Qbit
+  | QuantumRegisters { numQRegs :: Nat }
+  | ClassicalRegisters { numCRegs :: Nat }
+  deriving (Show, Eq)
+@-}
+
+data TypeError = UsesInvalidArrayIndex deriving (Show, Eq)
+
+-- This type represents the result of determining the type of an
+-- expression, being either a valid type or one that is invalid due to one or more reasons.
+type TypeCalculationResult = Either TypeError TermType
+
+-- Takes a context under which to evaluate an expression, an
+-- expression, and returns the type of the evaluated expression if
+-- possible. Returns an error otherwise explaining why the type
+-- could not be determined
+determineType :: EvaluationContext -> Expression -> TypeCalculationResult
+determineType _ _ = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,21 @@
 import Test.Hspec
-import qualified Typecheck as T
+import Typecheck(Expression(..),
+      EvaluationContext,
+      determineType,
+      Identifier,
+      TermType(..),
+      )
+import qualified Data.Map as M
+
+-- Takes an association list of identifiers to their respective types and
+-- returns an equivalent context to evaluate expressions
+genContext :: [(Identifier, TermType)] -> EvaluationContext
+
+genContext = M.fromList
 
 main :: IO ()
 main = hspec $ do
   describe "Accessing elements from a collection of registers" $ do
     describe "Using a valid index to access a register" $ do
       it "Returns the content inside the register" $ do
-        pending
-        
-  
+        determineType (genContext [("x", QuantumRegisters 2)]) RegisterAccess{registerName = "x", registerNumber = 0} `shouldBe` Right Qbit

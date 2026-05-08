fb2deb6d4d5611e9970345526ac789aa309c101f
Eitan Barylko <eitan@barylko.com>
2026-05-07 17:50:42 -0700

    Changed the package.yaml

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import qualified Typecheck as T
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Accessing elements from a collection of registers" $ do
+    describe "Using a valid index to access a register" $ do
+      it "Returns the content inside the register" $ do
+        pending
+        
+  

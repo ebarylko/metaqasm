dd0a2aaeef26afe843d17ea1a330d43f7c5d75dd
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2026-05-13 23:31:32 -0700

    Accessing a collection of registers that is out of scope (#12)

--- src/Typecheck.hs
+++ src/Typecheck.hs
@@ -11,23 +11,26 @@ module Typecheck
       TypeError(..),
       Nat(..),
       Pos(..),
-      RegisterGroupInfo(..)
+      Index
     ) where
 
 import qualified Data.Map as M
 import Data.Function ((&))
-import Control.Monad (mfilter)
+import Control.Arrow ((>>>))
+import Data.List.NonEmpty
+
 
 -- This data type represents a natural number
 newtype Nat = Nat Int deriving (Eq, Show, Ord)
 
 type Index = Nat
 
-type Identifier = String
+type Identifier = NonEmpty Char
 
 -- This data type represents the context under which to evaluate
 -- the type of a term
 type EvaluationContext = M.Map Identifier TermType
+
 
 -- This data type represents the values an expression can take on,
 -- being either a reference to another term or an attempt to obtain a bit or qubit from a
@@ -39,42 +42,44 @@ data RegisterType = Quantum | Classical deriving (Show, Eq)
 
 newtype Pos = Pos Int deriving (Show, Eq, Ord)
 
--- This type represents the info dictating how many registers are being
--- used and of what kind
-data RegisterGroupInfo = RegisterGroupInfo RegisterType Pos deriving (Show, Eq)
-
--- This data type represents the possible types a term can take on, being a classical bit,
--- a quantum bit, or a collection of classical/quantum registers of size N, where N > 0
 data TermType
   = Bit
   | Qbit
-  | RegisterGroup RegisterGroupInfo
+  | RegisterGroup RegisterType Pos
   deriving (Show, Eq)
 
-data TypeError = UsesInvalidArrayIndex deriving (Show, Eq)
+data TypeError = UsesInvalidArrayIndex | VariableNotInScope Identifier deriving (Show, Eq)
 
 -- This type represents the result of determining the type of an
 -- expression, being either a valid type or one that is invalid due to one or more reasons.
 type TypeCalculationResult = Either TypeError TermType
+
+-- Takes an id referring to an expression, an evaluation scope, and returns the type of the referenced
+-- expression if it exists. Returns an error otherwise.
+findTypeWithinScope :: Identifier -> EvaluationContext -> Either TypeError TermType
+
+findTypeWithinScope varName = M.lookup varName >>> maybe (Left $ VariableNotInScope varName) Right
+
+eitherFromPred :: (a -> Bool) -> err -> a -> Either err a
+
+eitherFromPred predicate elseCase x = if predicate x then Right x else Left elseCase
 
 -- Takes a context under which to evaluate an expression, an
 -- expression, and returns the type of the evaluated expression if
 -- possible. Returns an error otherwise explaining why the type
 -- could not be determined
 determineType :: EvaluationContext -> Expression -> TypeCalculationResult
-determineType m (RegisterAccess{registerName, registerNumber}) = M.lookup registerName m >>= getRegGroupInfo & mfilter (isAccessingValidReg registerNumber) & maybe (Left UsesInvalidArrayIndex) getRegisterContentType
+determineType m (RegisterAccess{registerName, registerNumber}) =
+  findTypeWithinScope registerName m
+  >>= eitherFromPred (isAccessingValidReg registerNumber) UsesInvalidArrayIndex
+  & fmap getRegisterContentType
   where
-    isAccessingValidReg :: Index -> RegisterGroupInfo -> Bool
-    isAccessingValidReg (Nat registerIdx) (RegisterGroupInfo _ (Pos regCount)) = regCount > registerIdx
+    isAccessingValidReg :: Index -> TermType -> Bool
+    isAccessingValidReg (Nat registerIdx) (RegisterGroup _ (Pos regCount)) = regCount > registerIdx
 
-    getRegGroupInfo :: TermType -> Maybe RegisterGroupInfo
-
-    getRegGroupInfo (RegisterGroup x) = return x
-    getRegGroupInfo _ = Nothing
-
-    getRegisterContentType :: RegisterGroupInfo -> TypeCalculationResult
-    getRegisterContentType (RegisterGroupInfo Quantum _ ) = Right Qbit
-    getRegisterContentType (RegisterGroupInfo Classical _ ) = Right Bit
+    getRegisterContentType :: TermType -> TermType
+    getRegisterContentType (RegisterGroup Quantum _ ) = Qbit
+    getRegisterContentType (RegisterGroup Classical _ ) = Bit
 
 
 determineType _ _ = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,12 +8,13 @@ import Typecheck(Expression(..),
       TypeError(..),
       Nat(..),
       Pos(..),
-      RegisterGroupInfo(..)
+      Index
       )
 
 import qualified Data.Map as M
-import Test.QuickCheck 
+import Test.QuickCheck
 import Test.Hspec.QuickCheck
+import Data.List.NonEmpty as NL
 
 -- Takes an association list of identifiers to their respective types and
 -- returns an equivalent context to evaluate expressions
@@ -21,18 +22,15 @@ genContext :: [(Identifier, TermType)] -> EvaluationContext
 
 genContext = M.fromList
 
-positiveNum :: Gen Pos
-positiveNum = (Pos . getPositive) <$> (arbitrary :: Gen (Positive Int))
+instance Arbitrary RegisterType where
+  arbitrary = elements [Classical, Quantum]
 
-registerType :: Gen RegisterType
-registerType = oneof [return Classical, return Quantum]
+instance Arbitrary Pos where
+  arbitrary = Pos . getPositive <$> arbitrary
 
-
-registerGroupInfo :: Gen RegisterGroupInfo
-registerGroupInfo = RegisterGroupInfo <$> registerType <*> positiveNum
-
-instance Arbitrary RegisterGroupInfo where
-  arbitrary = registerGroupInfo
+-- This type represents the information needed to create a collection of
+-- registers, being the type of the registers and the number of registers.
+type RegisterGroupInfo = (RegisterType, Pos)
 
 -- This data type represents a specification describing a collection
 -- of quantum/classical registers of size N and a request to
@@ -40,20 +38,22 @@ instance Arbitrary RegisterGroupInfo where
 data RegAccessSpec = Spec RegisterGroupInfo Nat deriving (Show, Eq)
 
 
-
 -- Takes a register collection id A, the index of the register to
 -- access I, and returns an expression representing a request to
 -- access the Ith register of A
 accessNthRegister :: Identifier -> Nat -> Expression
-accessNthRegister name regIdx = RegisterAccess{registerName = name, registerNumber = regIdx}
+accessNthRegister = RegisterAccess
 
 
-prop_regAccessAlwaysValid :: RegAccessSpec -> IO ()
+instance Arbitrary a => Arbitrary (NonEmpty a) where
+  arbitrary = NL.fromList  <$> listOf1 arbitrary
+
+prop_regAccessAlwaysValid :: RegAccessSpec -> Identifier -> IO ()
 
 -- Tests that accessing the ith register from a collection of registers of
 -- size N, where N > i, always succeeds
-prop_regAccessAlwaysValid  (Spec specInfo@(RegisterGroupInfo regType _) regIdx) =
-  determineType (genContext [("x", RegisterGroup specInfo)]) (accessNthRegister "x" regIdx) `shouldBe` expectedRegisterContent
+prop_regAccessAlwaysValid  (Spec info@(regType, _) regIdx) regName =
+  determineType ctx regAccReq `shouldBe` expectedRegisterContent
   where
 
     expectedRegisterContent = (Right . calcContentType) regType
@@ -62,14 +62,20 @@ prop_regAccessAlwaysValid  (Spec specInfo@(RegisterGroupInfo regType _) regIdx) 
     calcContentType Classical = Bit
     calcContentType Quantum = Qbit
 
+    ctx = genContext [(regName, uncurry RegisterGroup info)]
+    regAccReq = accessNthRegister regName regIdx
 
-prop_regAccessAlwaysFails :: RegAccessSpec -> IO ()
+
+prop_regAccessAlwaysFails :: RegAccessSpec -> Identifier -> IO ()
 
 
 -- Tests that accessing the ith register from a collection of registers of
 -- size N, where N <= i, always fails
-prop_regAccessAlwaysFails  (Spec specInfo regIdx) =
-  determineType (genContext [("x", RegisterGroup specInfo)]) (accessNthRegister "x" regIdx) `shouldBe` Left UsesInvalidArrayIndex
+prop_regAccessAlwaysFails  (Spec info regIdx) regName =
+  determineType ctx regAccReq `shouldBe` Left UsesInvalidArrayIndex
+  where
+    ctx = genContext [(regName, uncurry RegisterGroup info)]
+    regAccReq = accessNthRegister regName regIdx
 
 -- Takes a function that modifies range of registers accessed
 -- in a specification and returns a generator that uses
@@ -77,7 +83,7 @@ prop_regAccessAlwaysFails  (Spec specInfo regIdx) =
 genRegAccessSpec :: (Int -> (Int, Int)) -> Gen RegAccessSpec
 
 genRegAccessSpec f = do
-  x@(RegisterGroupInfo _ (Pos v)) <- registerGroupInfo
+  x@(_, (Pos v)) <- arbitrary
   randIdx <- (chooseInt . f) v
   (return . Spec x) $  Nat randIdx
 
@@ -93,6 +99,20 @@ invalidRegAccessSpec :: Gen RegAccessSpec
 
 invalidRegAccessSpec = genRegAccessSpec $ \x -> (x, x + 50)
 
+instance Arbitrary Nat where
+  arbitrary = Nat . getNonNegative <$> arbitrary 
+
+-- Tests that accessing a register collection that is not in
+-- the current evaluation scope always fails.
+prop_cannotAccessOutOfScopeRegColl :: Identifier -> Index -> IO ()
+
+prop_cannotAccessOutOfScopeRegColl regName regIdx =
+  determineType emptyCtx regAccReq `shouldBe` (Left . VariableNotInScope) regName
+  where
+    emptyCtx = M.empty
+    regAccReq = accessNthRegister regName regIdx
+
+
 main :: IO ()
 main = hspec $ do
   describe "Accessing elements from a collection of registers of size N > 0" $ do
@@ -101,3 +121,7 @@ main = hspec $ do
 
     prop "Accessing the ith register where i >= N returns an error" $ do
       forAll invalidRegAccessSpec  prop_regAccessAlwaysFails 
+
+  describe "Accessing elements from a collection of registers that is out of scope" $ do
+    prop "Accessing any register returns an error stating the collection is not in scope" $ do
+      prop_cannotAccessOutOfScopeRegColl

47b7411a095ae26f987e73ba3425818bdb240d6d
Eitan Barylko <eitan@barylko.com>
2026-06-06 10:53:03 -0700

    Made a small change

--- src/Typecheck.hs
+++ src/Typecheck.hs
@@ -11,7 +11,8 @@ import qualified Data.Map as M
 import Control.Arrow ((>>>))
 import Syntax(Identifier,
               Expression(..),
-              WithContext(..))
+              WithContext(..),
+              Id)
 import Lexer(LineNumber(..))
 
 
@@ -43,16 +44,11 @@ type TypeCalculationResult = Either TypeErrAt TermType
 
 -- Takes an id referring to an expression, an evaluation scope, and returns the type of the referenced
 -- expression if it exists. Returns an error otherwise.
---findTypeWithinScope :: Identifier -> EvaluationContext -> TypeCalculationResult
+findTypeWithinScope :: Id -> EvaluationContext -> TypeCalculationResult
 
---findTypeWithinScope varName = M.lookup varName >>> maybe (Left $ VariableNotInScope varName) Right
-
--- Takes a predicate, a function to generate an err, the input, and
--- returns an error if the data does not satisfy the predicate. Returns the
--- data otherwise.
-eitherFromPred :: (a -> Bool) -> (a -> err) -> a -> Either err a
-
-eitherFromPred predicate elseCase x = if predicate x then Right x else (Left . elseCase) x
+findTypeWithinScope (WithContext varName lineNum) = M.lookup varName >>> maybe lookupErr Right
+  where
+    lookupErr = Left $ WithContext (VariableNotInScope varName) lineNum
 
 
 -- Takes a context under which to evaluate an expression, an
@@ -60,28 +56,8 @@ eitherFromPred predicate elseCase x = if predicate x then Right x else (Left . e
 -- possible. Returns an error otherwise explaining why the type
 -- could not be determined
 determineType :: EvaluationContext -> Expression -> TypeCalculationResult
-determineType m (RegisterAccess{registerName, registerNumber}) =
-  error "Need to implement the register access case"
---  findTypeWithinScope registerName m
---  >>= eitherFromPred isAccessingRegColl genMismatchInfo
---  >>= eitherFromPred (isAccessingValidReg registerNumber) (const UsesInvalidArrayIndex)
---  & fmap getRegisterContentType
---  where
---    isAccessingValidReg :: Index -> TermType -> Bool
---    isAccessingValidReg (Nat registerIdx) (RegisterGroup _ (Pos regCount)) = regCount > registerIdx
---    isAccessingValidReg _ _ = False
---
---    getRegisterContentType :: TermType -> TermType
---    getRegisterContentType (RegisterGroup Quantum _ ) = Qbit
---    getRegisterContentType (RegisterGroup Classical _ ) = Bit
---    getRegisterContentType _ = error "Should only have received a collection of registers"
---
---    isAccessingRegColl :: TermType -> Bool
---    isAccessingRegColl (RegisterGroup _ _)  = True
---    isAccessingRegColl _  = False
---
---    genMinRegCollSize (Nat v) = Pos $ v + 1
---    genMismatchInfo actType =  TypeMismatch registerName actType $ ExpectedAtLeastNRegs $ genMinRegCollSize registerNumber
+determineType m (RegisterAccess registerName _) =
+  findTypeWithinScope registerName m
 
 
 determineType _ _ = error "Have not implemented this yet"

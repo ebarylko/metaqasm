9a5323d2904952d6d958a4630d1cabc5ab81e30f
Eitan Barylko <eitan@barylko.com>
2026-06-14 13:27:39 -0700

    Changed a test

--- test/GrammarSpec.hs
+++ test/GrammarSpec.hs
@@ -1,22 +1,39 @@
+{-# LANGUAGE TypeApplications #-}
 module GrammarSpec(spec) where
 
 import Test.Hspec
 import Grammar(parseText)
 import Syntax(Expression(..),
            WithContext(..),
-           Identifier)
+           Identifier,
+           GateApp(..))
 import Lexer(LineNumber(..))
 import Typecheck(Term)
 import qualified Vary
+import Data.Maybe(fromJust)
 
 -- Takes a name for a variable, the line it was found, and constructs
 -- a MetaQASM term representing the variable.
-genVar :: Identifier -> LineNumber -> Term
+genVar :: Identifier -> LineNumber -> Expression
 
-genVar varName lineNum = Vary.from $ Var $ WithContext varName lineNum
+genVar varName lineNum =  Var $ WithContext varName lineNum
+
+type Gate = Expression -> GateApp
+
+-- Takes an expression, the gate to apply to it, and  and generates the
+-- MetaQASM term corresponding to the application of the gate to the
+-- expression
+genGateApp :: Gate -> Expression -> GateApp
+
+genGateApp = ($)
+
+toExpr = fromJust . Vary.into @Expression
 
 spec = do
-  describe "Parsing MetaQASM terms" $ do
+  describe "Parsing MetaQASM programs" $ do
     describe "Parsing variables" $ do
       it "Generates a variable with the context of where it was found" $ do
-        parseText "varName" `shouldBe` Right (genVar "varName" (LineNumber 1))
+        (fmap toExpr . parseText) "varName" `shouldBe` (Right .  genVar "varName") (LineNumber 1)
+--    describe "Parsing T dagger applications" $ do
+--      it "Generates a term representing the application" $ do
+--        parseText "tdg(varName)" `shouldBe` (Right  . genGateApp Tdg) (genVar "varName" (LineNumber 1))

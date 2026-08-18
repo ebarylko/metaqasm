2bc257437b17de02895c9360b0f073f37559243d
Eitan Barylko <eitan@barylko.com>
2026-08-18 13:32:13 -0700

    Added parentheses

--- src/Typecheck.hs
+++ src/Typecheck.hs
@@ -354,8 +354,10 @@ verifyParametricExpr m validIdxVars RegisterAccess{registerName, registerNumber}
     isAccessingValidReg :: Idx -> [IndexVar] -> TermType -> ExceptT TypeErrAt IO Bool
     isAccessingValidReg wantedIdx inScopeIdxVars (RegisterGroup _ numOfRegs) = (proveAccessIsValid inScopeIdxVars `on` extractVal)  wantedIdx numOfRegs
     proveAccessIsValid :: [IndexVar] -> Index -> Index -> ExceptT TypeErrAt IO Bool
-    proveAccessIsValid idxVarsInUse wantedIdx regCount =  G.solve G.z3 $ assumeIdxVarsAreNonNeg idxVarsInUse `G.symImplies` (valueOf wantedIdx) `isBoundedExclusivelyBy` (valueOf regCount)
+    proveAccessIsValid idxVarsInUse wantedIdx regCount =  ExceptT $ fmap (error "Have not implemented left branch yet") (error "Have not implemented right branch yet") $ solveUsingZ3 $ G.symNot $ assumeIdxVarsAreNonNeg idxVarsInUse `G.symImplies` ((valueOf wantedIdx) `isBoundedExclusivelyBy` (valueOf regCount))
 
+solveUsingZ3 :: G.SymBool -> IO (Either G.SolvingFailure G.Model)
+solveUsingZ3 = G.solve G.z3
 
 -- Takes two numbers, a, b, and generates a condition checking
 -- that a is in [0, b)
